8abc62c8d6b9923e43a276c68df09ff25115901d
manekineko1234 <181326568+manekineko1234@users.noreply.github.com>
2025-03-13 09:59:00 +0900

    ボタン、スピーカー追加

--- speaker.py
+++ speaker.py
@@ -1,42 +1,53 @@
 #　土台
-
 import RPi.GPIO as GPIO
 import time
-
+import socket
 
 # スピーカーのGPIOピン番号
 SPEAKER_PIN = 12
+BUTTON = 3
 
 
 # GPIOの設定
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SPEAKER_PIN, GPIO.OUT)
+GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-
+# classつけた
 # スピーカーをオンにする関数
-def speaker_on():
-    GPIO.output(SPEAKER_PIN, GPIO.HIGH)
-    print("スピーカーオン")
+class Speaker:
+    def __init__(self):
+        self.meido = 0
+        self.y = 1
+        self.burocas = ('localhost', 8890)
+        self.sock = socket.socket(socket.AF_INET, type=socket.SOCK_DGRAM)         
+
+    def speaker_on(self):
+        GPIO.output(SPEAKER_PIN, GPIO.HIGH)
+        print("スピーカーオン")
 
 
 # スピーカーをオフにする関数
-def speaker_off():
-    GPIO.output(SPEAKER_PIN, GPIO.LOW)
-    print("スピーカーオフ")
+    def speaker_off(self):
+        GPIO.output(SPEAKER_PIN, GPIO.LOW)
+        print("スピーカーオフ")
+
+#スピーカーを制御するインスタンスを作成
+speaker = Speaker()
 
  # ボタンの状態を監視してスピーカーを制御するループ
 try:
     while True:
         input_state = GPIO.input(BUTTON)
         if input_state == GPIO.LOW:  # ボタンが押された場合
-            speaker_off()
-            if meido == y:
-                sock.sendto(b'tomareya'.encode(encoding='utf-8'), burocas)
+            speaker.speaker_off()
+            if speaker.meido == speaker.y:
+                speaker.sock.sendto(b'tomareya'.encode(encoding='utf-8'), speaker.burocas)
             print(f'停止ボタン押下')
             button = 0
             break  # いったん検知したらbreak 継続動作するよう書き換え
         else:
-            speaker_on()
+            speaker.speaker_on()
         time.sleep(0.1)  # 0.1秒待機
 except KeyboardInterrupt:
     pass
