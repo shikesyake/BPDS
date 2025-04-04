b21c84f938177f72f6271a004822d5d9c2627e7f
shikesyake <shike4419@gmail.com>
2025-04-04 22:53:58 +0900

    bind使えない問題発生

--- koki.py
+++ koki.py
@@ -2,6 +2,8 @@ import socket
 import time
 from send import P2P
 from button import GPIO
+import readchar
+#from readchar import readkey, key as inchar
 
 ##############################
 ##                         　##
@@ -9,16 +11,24 @@ from button import GPIO
 ##                  　　      ##
 #############################
 
-raspi = True
+
+raspi = False
+keyboard = True
+p2p = P2P()
+
+
 try:
     gpio = GPIO()
-    p2p = P2P()
     raspi = True
     print('GPIOが存在するデバイスです')
 except:
     raspi = False
     print('GPIOが存在しないデバイスです')
-
+    try:
+        inputkey = readchar.readchar()
+    except:
+        keyboard = False
+        print('キー操作不可')
 
 M_SIZE = 1024
 
@@ -33,7 +43,9 @@ sock.settimeout(0.1)
 print('create socket')
 
 # ②自ホストで使用するIPアドレスとポート番号を指定
-sock.bind(locaddr)
+##送信する場合はバインドを外す
+
+#sock.bind(locaddr)
 print('Waiting message')
 
 
@@ -44,9 +56,18 @@ while True:
             p2p.stop_alert()
             print('停止ボタン押下')
         gpio.tick()
+    elif keyboard == True:
+        inputkey = readchar.readchar()
+        if inputkey == "t":
+            p2p.stop_alert()
+            print('停止ボタン押下')
+            #time.sleep(0.1)
+        elif inputkey == "s":
+            p2p.alert()
+            print('アラートボタン押下')  
     try:
         # ③Clientからのmessageの受付開始
-        time.sleep(0.2)
+        #time.sleep(0.2)
         message, cli_addr = sock.recvfrom(M_SIZE)
         message = message.decode(encoding='utf-8')
         print(f"{cli_addr}から"f'[{message}]を受信しました')
