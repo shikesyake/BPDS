3264f96767b0b52eb9b78176b2ef9dd96add9698
shikesyake <shike4419@gmail.com>
2025-04-07 09:27:24 +0900

    キー操作と受信を両立 不要な関数を削除 送信元が自機ならfromを表示しない

--- koki.py
+++ koki.py
@@ -2,8 +2,7 @@ import socket
 import time
 from send import P2P
 from button import GPIO
-import readchar
-#from readchar import readkey, key as inchar
+import keyboard as kb
 
 ##############################
 ##                         　##
@@ -12,9 +11,11 @@ import readchar
 #############################
 
 
-raspi = False
-keyboard = True
 p2p = P2P()
+# GPIOピンの有無
+raspi = False
+#自分が送信したか
+sendby = False
 
 # GPIOの有無を判定
 try:
@@ -24,14 +25,8 @@ try:
 except:
     raspi = False
     print('GPIOが存在しないデバイスです')
-    keyboard = True
-
 
 M_SIZE = 1024
-
-kensyutu = 0
-button = 0
-# 
 locaddr = ('0.0.0.0', 8890)
 burocas = ('255.255.255.255',8890)
 # ①ソケットを作成する
@@ -41,48 +36,46 @@ print('create socket')
 
 # ②自ホストで使用するIPアドレスとポート番号を指定
 ##送信する場合はバインドを外す
-
-sock.bind(("localhost",8890)) # type: ignore
+sock.bind(locaddr)
 print('Waiting message')
 
-
-#if kensyutu == 1:
 while True:
     if raspi == True:
         if gpio.is_pressed():
             p2p.stop_alert()
             print('停止ボタン押下')
         gpio.tick()
-
-
-    elif keyboard == True:
-        inputkey = readchar.readchar()
-        if inputkey == "t":
-            p2p.stop_alert()
-            print('停止ボタン押下')
-            #time.sleep(0.1)
-        elif inputkey == "s":
-            p2p.alert()
-            print('アラートボタン押下')  
-
+    
+    if kb.is_pressed("escape"):
+        p2p.stop_alert()
+        sendby = True
+        print('停止ボタン押下')
+        time.sleep(0.1)
+    elif kb.is_pressed("space"):
+        p2p.alert()
+        sendby = True
+        print('アラートボタン押下')
+        time.sleep(0.1)
     try:
         # ③Clientからのmessageの受付開始
         #time.sleep(0.2)
         message, cli_addr = sock.recvfrom(M_SIZE)
         message = message.decode(encoding='utf-8')
-        print(f"{cli_addr}から"f'[{message}]を受信しました')
-
-        if message == "tuitade":
-            print('親機が起動しました')
-        elif message == "akan":
-            print('ALERT受信')
-            if raspi:
-                gpio.on()
-        elif message == "tomareya":
-            print('STOP受信')
-            if raspi:
-                gpio.off()
-                
+        if sendby == False:
+            print(f"{cli_addr}から"f'[{message}]を受信しました')
+            if message == "tuitade":
+                print('親機が起動しました')
+            elif message == "akan":
+                print('ALERT受信')
+                if raspi:
+                    gpio.on()
+            elif message == "tomareya":
+                print('STOP受信')
+                if raspi:
+                    gpio.off()
+        else:
+            sendby = False
+            time.sleep(0.1)
     except socket.timeout:
         continue
     except KeyboardInterrupt:
